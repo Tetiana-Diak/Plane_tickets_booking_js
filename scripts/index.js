4d09539d815e6c8fbbcac4a3e7f3782415c86388
Tetiana-Diak <56723516+Tetiana-Diak@users.noreply.github.com>
2022-02-07 17:19:32 +0200

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+import start from "./modules/start.js";
+import getFomrPerson from "./modules/formPerson.js";
+import readyPlane from "./modules/readyPlane.js";
+import getData from "./servis/getTour.js";
+
+
+const init = async (selectorApp, title) => {
+    const app = document.querySelector(selectorApp);
+    const data = await getData();
+    const {main, firstForm, h1} = start (app, title, data);
+
+    firstForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const tourData = data.find(tour => tour.id === firstForm.tour.value);
+
+        h1.textContent = tourData.tour;
+        const forms = getFomrPerson(firstForm.count.value);
+
+        firstForm.remove();
+
+        main.append(...forms);
+
+        readyPlane(forms, main, tourData);
+    });
+};
+
+init('.app', 'Выберите тур');
+
